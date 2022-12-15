a6f8f7de35973dcbab7984412cc5af42b7d123d6
Vic <vontiverosewhs@gmail.com>
2022-12-16 00:36:42 +0200

    First 20 Pokemon are rendered with sprite and dex number

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import PokeList from './PokeList/PokeList';
 import axios from 'axios';
 import './App.css';
-import { useEffect } from 'react';
 
 function App() {
   const [ pokedata, setPokedata ] = useState([]);
 
   useEffect(() => {
     axios.get('https://pokeapi.co/api/v2/pokemon').then((res) => {
-      setPokedata(res.data.results)
+      setPokedata(res.data.results);
+      console.log(pokedata);
     })
   }, []);
 
--- src/PokeList/PokeList.jsx
+++ src/PokeList/PokeList.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import './PokeList.css';
 
 function PokeList(props) {
-    const pokedata = props.pokedata;
+    const { pokedata } = props;
+    const [ pokemonData, setPokemonData ] = useState([]);
+
+    useEffect(() => {
+        pokedata.map((pokemon) => {
+            axios.get(pokemon.url).then((res) => {
+                setPokemonData(current => [...current, res.data]);
+            })
+        })
+    }, []);
+
     return (
         <>
-            {pokedata.map((pokemon) => {
+            {pokemonData.map((pokemon) => {
                 return (
-                    <>
-                        <p key={pokemon.name}>{pokemon.name}</p>
-                        <a href={pokemon.url}>More info</a>
-                    </>
+                    <div key={pokemon.id} className="pokemon-list">
+                        <p>{pokemon.id}.</p>
+                        <img src={pokemon.sprites.front_default} />
+                        <p>{(pokemon.name).charAt(0).toUpperCase() + (pokemon.name).slice(1)}</p>
+                    </div>
                 )
             })}
         </>
